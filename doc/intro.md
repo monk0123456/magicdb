16d238e27e08dc3250d36731086e026090d7c88b
chenfei <monk0123456@126.com>
2022-10-26 23:30:23 +0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to magicdb
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/magicdb/core_test.clj
@@ -0,0 +1,7 @@
+(ns magicdb.core-test
+  (:require [clojure.test :refer :all]
+            [magicdb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
